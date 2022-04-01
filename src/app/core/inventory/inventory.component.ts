acaa5c0fb9c985879620569440b5ad028770c889
miha.novak1024 <miha.novak1024@gmail.com>
2022-04-01 13:11:23 +0200

    Initial inventory screen

--- src/app/core/inventory/inventory.component.ts
+++ src/app/core/inventory/inventory.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { InventoryData, Item, UnitType } from '../model/inventory-data.model';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
 })
 export class InventoryComponent implements OnInit {
+  data: InventoryData;
+  selected: Item | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dataService: DataService) {
+    this.data = dataService.inventoryData;
   }
 
+  ngOnInit(): void {}
+
+  isNonNumberUnitType(): boolean {
+    if (this.selected) {
+      return this.selected.quantityInfo.type != UnitType.NUMBER;
+    }
+    return false;
+  }
+
+  getDimensions(): String {
+    if (this.selected) {
+      const  dimensions = this.selected.quantityInfo.dimensions;
+      if (dimensions) {
+        return JSON.stringify(dimensions);
+      }
+    }
+    return "";
+  }
+
+  onSelected(item: Item) {
+    this.selected = item;
+  }
 }
